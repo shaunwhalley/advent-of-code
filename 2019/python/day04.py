bcc3b4da295fba8fad0631b318305c54b2423ade
shaunwhalley <shaun.whalley@bbc.co.uk>
2021-10-02 21:50:00 +0100

    day 4 - refactor

--- 2019/python/day04.py
+++ 2019/python/day04.py
@@ -1,18 +1,18 @@
 input_start = 234208
 input_end = 765869
 
-def meets_rules(number, exact_pair):
+def meets_rules(number, group_match_func):
     digits = list(str(number))
 
     if not digits == sorted(digits): return False
 
     for d in set(digits):
-        if (exact_pair and digits.count(d) == 2) or (not exact_pair and digits.count(d) >= 2): return True
+        if group_match_func(digits.count(d)): return True
 
     return False
 
-part_one = len([n for n in range(input_start, input_end + 1) if meets_rules(number = n, exact_pair = False)])
-part_two = len([n for n in range(input_start, input_end + 1) if meets_rules(number = n, exact_pair = True)])
+part_one = len([pwd for pwd in range(input_start, input_end + 1) if meets_rules(pwd, lambda n: n >= 2)])
+part_two = len([pwd for pwd in range(input_start, input_end + 1) if meets_rules(pwd, lambda n: n == 2)])
 
 print(part_one)  # 1246
 print(part_two)  # 814
