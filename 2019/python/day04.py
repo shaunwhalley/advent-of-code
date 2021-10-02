9f92467cab5127e6ff722657a1d9e60cb73cce8f
shaunwhalley <shaun.whalley@bbc.co.uk>
2021-10-02 21:41:37 +0100

    day 4 - refactor

--- 2019/python/day04.py
+++ 2019/python/day04.py
@@ -1,33 +1,18 @@
 input_start = 234208
 input_end = 765869
 
-def meets_rules_one(number):
-    digits = [int(n) for n in list(str(number))]
+def meets_rules(number, exact_pair):
+    digits = list(str(number))
 
-    for idx in range(len(digits) - 1):
-        if digits[idx + 1] < digits[idx]: return False
+    if not digits == sorted(digits): return False
 
-    for idx in range(len(digits) - 1):
-        if digits[idx] == digits[idx + 1]: return True
-
-    return False
-
-def meets_rules_two(number):
-    digits = [int(n) for n in list(str(number))]
-
-    for idx in range(len(digits) - 1):
-       if digits[idx + 1] < digits[idx]: return False
-
-    for n in range(0, 10):
-        if digits.count(n) == 2: return True
+    for d in set(digits):
+        if (exact_pair and digits.count(d) == 2) or (not exact_pair and digits.count(d) >= 2): return True
 
     return False
 
-
-part_one = len([n for n in range(input_start, input_end + 1) if meets_rules_one(n)])
-
-part_two = len([n for n in range(input_start, input_end + 1) if meets_rules_two(n)])
+part_one = len([n for n in range(input_start, input_end + 1) if meets_rules(number = n, exact_pair = False)])
+part_two = len([n for n in range(input_start, input_end + 1) if meets_rules(number = n, exact_pair = True)])
 
 print(part_one)  # 1246
-
 print(part_two)  # 814
