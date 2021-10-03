981ef55f4124ff9c2416304bda6098ccf85e5f78
shaunwhalley <shaun.whalley@bbc.co.uk>
2021-10-03 18:38:35 +0100

    day 4 - refactor

--- 2019/javascript/day04.js
+++ 2019/javascript/day04.js
@@ -1,31 +1,28 @@
-const input_start = 234208
-const input_end = 765869
-
-const meetsRules = (number, groupMatchFunc) => {
-  const digits = String(number).split('')
-
-  if (Number(digits.sort().join('')) !== number) return false
-
-  for (const d of new Set(digits)) {
-    const count = digits.reduce((t, n) => (n === d ? t + 1 : t), 0)
-    if (groupMatchFunc(count)) return true
-  }
-
-  return false
-}
-
-const numberOfMatches = (matcher) => {
+const numberOfMatches = (groupMatchFunc) => {
+  const input_start = 234208
+  const input_end = 765869
   let matches = 0
 
-  for (n = input_start; n <= input_end; n++) {
-    if (meetsRules(n, matcher)) matches++
+  for (num = input_start; num <= input_end; num++) {
+    const digits = String(num).split('')
+    const uniqueDigits = [...new Set(digits)]
+
+    if (Number(digits.sort().join('')) !== num) continue
+
+    uniqueDigits.some((d) => {
+      const count = digits.reduce((t, n) => (n === d ? t + 1 : t), 0)
+      if (groupMatchFunc(count)) {
+        matches++
+        return true
+      }
+    })
   }
 
   return matches
 }
 
-const part_one = numberOfMatches((n) => n >= 2)
-const part_two = numberOfMatches((n) => n == 2)
+const partOne = numberOfMatches((n) => n >= 2)
+const partTwo = numberOfMatches((n) => n == 2)
 
-console.log(part_one) // 1246
-console.log(part_two) // 814
+console.log(partOne) // 1246
+console.log(partTwo) // 814
