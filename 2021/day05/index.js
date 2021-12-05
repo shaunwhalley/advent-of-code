0f90a884d0da666beb09ae8bc1dd7a159582694e
shaunwhalley <shaun.whalley@bbc.co.uk>
2021-12-05 16:23:30 +0000

    day 5 part 2

--- 2021/day05/index.js
+++ 2021/day05/index.js
@@ -1,22 +1,40 @@
 const readInput = require('../readInput')
 
-const partOne = () => {
-  const inputs = readInput(__dirname).split('\n')
-  const posRegEX = new RegExp(/\d+/g)
+const inputs = readInput(__dirname, false).split('\n')
+const posRegEX = new RegExp(/\d+/g)
+
+const partOne = (includeDiagonals) => {
+
   const overlaps = []
 
   inputs.forEach(input => {
     const [startX, startY, endX, endY] = input.match(posRegEX).map(Number)
-    const isDiagonal = startX !== endX && startY !== endY
-    if (isDiagonal) return
 
-    const isHorizontal = startX !== endX
-    const [start, end] = (isHorizontal ? [startX, endX] : [startY, endY]).sort((a, b) => a - b)
+    if (startY === endY) {
+      const [start, end] = [startX, endX].sort((a, b) => a - b)
 
-    for (let n = start; n <= end; n++) {
-      const [x, y] = isHorizontal ? [n, startY] : [startX, n]
-      if (!overlaps[y]) overlaps[y] = []
-      overlaps[y][x] = (overlaps[y][x] || 0) + 1
+      for (let n = start; n <= end; n++) {
+        const [x, y] = [n, startY]
+        if (!overlaps[y]) overlaps[y] = []
+        overlaps[y][x] = (overlaps[y][x] || 0) + 1
+      }
+    } else if (startX === endX) {
+      const [start, end] = [startY, endY].sort((a, b) => a - b)
+
+      for (let n = start; n <= end; n++) {
+        const [x, y] = [startX, n]
+        if (!overlaps[y]) overlaps[y] = []
+        overlaps[y][x] = (overlaps[y][x] || 0) + 1
+      }
+    } else if (includeDiagonals) {
+      const diff = Math.abs(startX - endX)
+
+      for (let n = 0; n <= diff; n++) {
+        const x = startX > endX ? startX - n : startX + n
+        const y = startY > endY ? startY - n : startY + n
+        if (!overlaps[y]) overlaps[y] = []
+        overlaps[y][x] = (overlaps[y][x] || 0) + 1
+      }
     }
   })
 
@@ -24,3 +42,4 @@ const partOne = () => {
 }
 
 console.log(partOne()) // 7644
+console.log(partOne(true)) // 12
