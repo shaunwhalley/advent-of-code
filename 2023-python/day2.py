cfa2e74c0581c4803cab348c7996958898952aab
shaun <shaunwhalley@users.noreply.github.com>
2023-12-03 23:03:53 +0000

    Refactor part 2

--- 2023-python/day2.py
+++ 2023-python/day2.py
@@ -9,21 +9,23 @@ def get_answer(inputFilePath):
     for line in input:
       game=int(re.search('Game ([0-9]+)', line).group(1))
       cube_pulls=re.findall('(([0-9]+) (red|green|blue))', line)
+      cube_pulls_possible = []
       minimums = { 'red': 0, 'green': 0, 'blue': 0 }
-
-      is_possible=all([int(cubes[1]) <= config[cubes[2]] for cubes in cube_pulls])
-
-      if is_possible: partOneTotal += game
 
       for cubes in cube_pulls:
         number = int(cubes[1])
         colour = cubes[2]
+
+        cube_pulls_possible.append(number <= config[cubes[2]])
 
         if colour in minimums and minimums[colour]:
           if number > minimums[colour]:
             minimums[colour] = number
         else:
           minimums[colour] = number
+
+      if all(cube_pulls_possible):
+        partOneTotal += game
 
       partTwoTotal += (minimums['red'] * minimums['green'] * minimums['blue'])
 
