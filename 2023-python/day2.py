e2e3888d9b3500ac50c09fd77545bf438ca525a6
shaun <shaunwhalley@users.noreply.github.com>
2023-12-03 14:34:17 +0000

    Refactor part 1

--- 2023-python/day2.py
+++ 2023-python/day2.py
@@ -1,16 +1,19 @@
 import re
 
-config = { 'red': 12, 'green': 13, 'blue': 14 }
+def get_answer(inputFilePath):
+  config = { 'red': 12, 'green': 13, 'blue': 14 }
+  total=0
 
-total=0
+  with open(inputFilePath) as input:
+    for line in input:
+      game=int(re.search('Game ([0-9]+)', line).group(1))
+      cube_pulls=re.findall('(([0-9]+) (red|green|blue))', line)
 
-with open('day2-part1-example.txt') as input:
-  for line in input:
-    game=int(re.search('Game ([0-9]+)', line).group(1))
-    cube_pulls=re.findall('(([0-9]+) (red|green|blue))', line)
+      is_possible=all([int(cubes[1]) <= config[cubes[2]] for cubes in cube_pulls])
 
-    is_possible=all([int(cubes[1]) <= config[cubes[2]] for cubes in cube_pulls])
+      if is_possible: total += game
 
-    if is_possible: total += game
+  return total
 
-print(total)
+# 3099
+print(get_answer('day2-input.txt'))
